f2ea45e798dbc3fbbfb7d9700088de5a28b7ceae
rughwed <RUGHWEDDKINI@GMAIL.com>
2025-01-21 14:05:30 +0530

    done

--- python/user.py
+++ python/user.py
@@ -1,15 +1,23 @@
 import sys
-
-class User():
-    
-    def __init__(self,**dict1):
+ 
+ 
+class User:
+ 
+    def __init__(self, **dict1):
         self.user = dict1
-        self.id = dict1['email']
-        self.__passw = dict1['Password']
+        self.id = dict1["Mobile No"]
+        self.email = dict1["Email"]
+        self.__passw = dict1["Password"]
         self.lg_status = False
-
-    def login(self,id,passw):
-        if id == self.id:
+ 
+    def login(self):
+        id = input("Enter mobile no: ")
+        if id.isdigit():
+            id = int(id)
+        else:
+            id = str(id)
+        passw = input("Enter password:")
+        if id == self.id or id == self.email:
             if passw == self.__passw:
                 print("successfully loggedin")
                 self.lg_status = True
@@ -17,12 +25,18 @@ class User():
                 print("wrong password")
         else:
             print("wrong username")
-    
-    def bookTicket(self,to,froml):
+ 
+    def bookTicket(self):
+        self.from1 = input("Enter source destination: ")
+        self.to = input("Enter destination location:")
         try:
             if self.lg_status:
-                print(f"ticket confirmed from {str(froml).title} to {str(to).title}  ")
+                print(
+                    f"ticket confirmed from {self.from1.title()} to {self.to.title()}  "
+                )
             else:
                 raise Exception("not loggedin")
         except:
             print(sys.exc_info())
+ 
+ 
